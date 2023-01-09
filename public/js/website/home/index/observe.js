0176a70522ffcabbb6ecbe5695d78b168f55e981
Ricardo García Jiménez <ricardo@genotipo.com>
2023-01-09 11:51:17 -0600

    feat: Elimina shadow de zonas oscuras

--- public/js/website/home/index/observe.js
+++ public/js/website/home/index/observe.js
@@ -9,10 +9,12 @@ const sectionObserver = new IntersectionObserver((entries, sectionObserver) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       headerNav.classList.add('bg-transparent')
+      headerNav.classList.remove('bg-white', 'shadow-md')
       headerNav.querySelector('.bar').classList.remove('text-ssbajio-gray-dark-1')
       headerNav.querySelector('.bar').classList.add('text-white')
     } else {
       headerNav.classList.remove('bg-transparent')
+      headerNav.classList.add('bg-white', 'shadow-md')
       headerNav.querySelector('.bar').classList.remove('text-white')
       headerNav.querySelector('.bar').classList.add('text-ssbajio-gray-dark-1')
     }
